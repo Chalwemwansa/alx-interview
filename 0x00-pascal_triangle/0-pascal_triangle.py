412f0bd8b50a367be9218cfe1a24b8dca39e0bd7
Chalwemwansa <132388927+Chalwemwansa@users.noreply.github.com>
2024-04-12 02:57:10 +0000

    implemetnted the pascals triangl last minute from github codespace, how cool

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""this module contains a function
+that returns the pascals triangle"""
+
+
+def pascal_triangle(n):
+    """this is the function that conatins the function implimenattion"""
+    if (n <= 0):
+        return ([])
+    main_list = [[1]]
+    for i in range(0, (n - 1)):
+        sub_list = [1]
+        for j in range(0, i):
+            sub_list.append(main_list[i][j] + main_list[i][j + 1])
+        sub_list.append(1)
+        main_list.append(sub_list)
+    return (main_list)
+
+
+if __name__ == "__main__":
+    for row in pascal_triangle(5):
+        print("[{}]".format(",".join([str(x) for x in row])))
