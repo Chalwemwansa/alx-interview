ba3db048d2ef2667e7fb58a9eafa5ae395219cf2
chalwe mwansa <chalwemwansa23@gmail.com>
2024-04-19 02:13:54 +0200

    made the file and added logic for the boxes alx interview question

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""module contains a function that checks if the boxes
+    list of lists can unlock all other boxes"""
+
+
+def canUnlockAll(boxes):
+    """the function that checks if boxes can be opened
+      make a list and add all the unique keys we have in the different
+      boxes and then check the range"""
+    if len(boxes) == 0:
+        return False
+    lst = boxes[0]
+    for key in lst:
+        if key in range(1, len(boxes)):
+            for i in boxes[key]:
+                if i in range(1, len(boxes)) and i not in lst:
+                    lst.append(i)
+    if __name__ == '__main__':
+        lst.sort()
+        print(lst)
+    if len(lst) == len(boxes) - 1:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    """True True False expected for the three different examples below"""
+    print(canUnlockAll([[1], [2], [3], [4], []]))
+    print(canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1],
+                       [6]]))
+    print(canUnlockAll([[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]))
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,1 @@
+this repo contains a file with solution to an alx interview
