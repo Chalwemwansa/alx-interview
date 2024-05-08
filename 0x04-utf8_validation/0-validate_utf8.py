9807de718fdefa3900ea9cd45a543ac0e5b01240
chalwe mwansa <chalwemwansa23@gmail.com>
2024-05-08 18:32:16 +0200

    checking

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
-"""this module contains the validUTF8 function"""
+"""checks if a given sequence contains valid utf8 data"""
 
 
 def validUTF8(data):
-    """function checks if the list passed in contains
-    only valid utf8 characters"""
-    for integer in data:
-        more_than = integer > 1114111
-        less_than = integer <= 0
-        invalid = (integer & 255) == 0
-        if more_than or less_than or invalid:
-            return False
-    return True
+    """function checks if a given byte in the list is a valid unicode
+    character"""
+    remaining_bytes = 0
+
+    for num in data:
+        if remaining_bytes == 0:
+            mask = 0x80
+            while mask & num:
+                remaining_bytes += 1
+                mask >>= 1
+
+            if remaining_bytes == 0:
+                continue
+
+            if remaining_bytes == 1 or remaining_bytes > 4:
+                return False
+
+        else:
+            if not (num & 0b10000000 and not num & 0b01000000):
+                return False
+
+        remaining_bytes -= 1
+
+    return remaining_bytes == 0
