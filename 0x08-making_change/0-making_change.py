dbd638ef0221130f88efaa4e4fa51a7fbcced8e0
chalwe mwansa <chalwemwansa23@gmail.com>
2024-06-07 03:45:58 +0200

    made the required file and added the needeededfunctionality

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module contains a function that determines the feweest number
+of coins needed to make change where
+the list of coins is given as the first argument and the amount
+is given as the second argument, the function returns -1 if it is not possible
+to give change."""
+
+def makeChange(coins, total):
+    """function that determines the feweest number
+    of coins needed to make change where
+    the list of coins is given as the first argument and the amount
+    is given as the second argument, the function returns -1 if it is not possible
+    to give change."""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        if total >= coin:
+            coin_count += total // coin
+            total = total % coin
+    if total == 0:
+        return coin_count
+    return -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+this folder contains a file that shows the implementation of the greedy algorithm  for chosing the least number of coins needed to make up change for a given amont passed as an argument
