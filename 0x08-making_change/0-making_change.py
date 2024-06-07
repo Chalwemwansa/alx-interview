c70347d2ecb92d235d99c890ba11320af7489523
chalwe mwansa <chalwemwansa23@gmail.com>
2024-06-07 03:48:40 +0200

    made the file and altered it a bit so that is follows pycodestyle format

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,12 +5,13 @@ the list of coins is given as the first argument and the amount
 is given as the second argument, the function returns -1 if it is not possible
 to give change."""
 
+
 def makeChange(coins, total):
     """function that determines the feweest number
     of coins needed to make change where
     the list of coins is given as the first argument and the amount
-    is given as the second argument, the function returns -1 if it is not possible
-    to give change."""
+    is given as the second argument, the function returns -1 if it is not
+    possible to give change."""
     if total <= 0:
         return 0
     coins.sort(reverse=True)
